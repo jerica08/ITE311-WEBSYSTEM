161f8dea7d42f589447fed4fdcfd219165ae854c
jerica08 <jericamarquez88@gmail.com>
2025-08-29 17:03:42 +0800

    updated the routes to include routes for your new authentication pages

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,5 +9,12 @@ $routes->get('home', 'Home::index');
 $routes->get('/', 'Home::index');
 $routes->get('/about', 'Home::about');
 $routes->get('/contact', 'Home::contact');
+$routes->get('/register', 'Auth::register');
+$routes->post('/register', 'Auth::register');
+$routes->get('/login', 'Auth::login');
+$routes->post('/login', 'Auth::login');
+$routes->get('/logout', 'Auth::logout');
+$routes->get('/dashboard', 'Auth::dashboard');
 $routes->setAutoRoute(true);
+
 
