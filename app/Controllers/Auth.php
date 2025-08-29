0f9f48ed249fb1285b99b824017f233705a06288
jerica08 <jericamarquez88@gmail.com>
2025-08-29 10:45:22 +0800

    created an initial method for register, login, logout, and dashboard

---
+++ app/Controllers/Auth.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+
+use CodeIgniter\Controller;
+
+class Auth extends Controller
+{
+    public function register()
+    {
+        
+    }
+
+    public function login(){
+
+    }
+
+    public function logout(){
+        
+    }
+
+    public function dashboard(){
+        
+    }
+}
