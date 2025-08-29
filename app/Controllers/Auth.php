0f581e7476ac092dc6ec4fd8f078d02f374e74cc
jerica08 <jericamarquez88@gmail.com>
2025-08-29 11:46:21 +0800

    created the register method

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -9,7 +9,32 @@ class Auth extends Controller
 {
     public function register()
     {
-        
+        helper(['form']);
+        $data = [];
+
+        if ($this->request->getMethod() === 'post'){
+            $rules = [
+               'name'     => 'required|min_length[3]|max_length[20]',
+                'email'    => 'required|valid_email|is_unique[users.email]',
+                'password' => 'required|min_length[6]|max_length[200]',
+            ];
+            if ($this->validate($rules)) {
+                $model = new UserModel();
+
+                $model->save([
+                    'name'     => $this->request->getVar('name'),
+                    'email'    => $this->request->getVar('email'),
+                    'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT)
+                ]);
+
+                return redirect()->to('/auth/login');
+            } else {
+                $data['validation'] = $this->validator;
+            }
+        }
+
+        return view('auth/register', $data);
+        }
     }
 
     public function login(){
