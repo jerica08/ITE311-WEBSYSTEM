dc6aff79c617f4b8f6cd2759e9202977262164be
jerica08 <jericamarquez88@gmail.com>
2025-08-29 14:31:33 +0800

    created a register file

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -10,29 +10,31 @@ class Auth extends Controller
     public function register()
     {
         helper(['form']);
+        $db =\Config\Database::connect();
         $data = [];
 
-        if ($this->request->getMethod() === 'post'){
+         if ($this->request->getMethod() === 'post'){
             $rules = [
                'name'     => 'required|min_length[3]|max_length[20]',
                 'email'    => 'required|valid_email|is_unique[users.email]',
                 'password' => 'required|min_length[6]|max_length[200]',
                 'password_confirm'=> 'matches[password]'
+                 
             ];
             if ($this->validate($rules)) {
-                $model = new UserModel();
                 
                 //HASH PASSWORD
-                $hashedPassword = password-hash($this->request->getVar('password'), PASSWORD_DEFAULT);
+                $hashedPassword = password_hash($this->request->getVar('password'), PASSWORD_DEFAULT);
 
                 //SAVE USERDATA
-                $model->save([
+                $builder = $db->table('users');
+                $builder->insert([
                     'name'     => $this->request->getVar('name'),
                     'email'    => $this->request->getVar('email'),
                     'password' => $hashedPassword,
                     'role'     => 'user'
                 ]);
-                
+
                 session()->setFlashdata('success','Registration successful! Please log in.');
                 return redirect()->to('/auth/login');
             } else {
@@ -41,8 +43,8 @@ class Auth extends Controller
         }
 
         return view('auth/register', $data);
-        }
     }
+    
 
     public function login(){
         helper(['form']);
