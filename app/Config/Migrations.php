3572baf348ac9dfaeff34b685169503e2d271064
jerica08 <jericamarquez88@gmail.com>
2025-08-31 12:53:07 +0800

    redo the lab4

--- app/Config/Migrations.php
+++ app/Config/Migrations.php
@@ -14,7 +14,7 @@ class Migrations extends BaseConfig
      * Migrations are enabled by default.
      *
      * You should enable migrations whenever you intend to do a schema migration
-     * and disable it back when you're done.
+     * and disable it back when you're done. property
      */
     public bool $enabled = true;
 
