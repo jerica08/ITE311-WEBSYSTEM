a9ce7c54dabc212be54b310bce5802e3a7d2b908
jerica08 <jericamarquez88@gmail.com>
2025-08-31 13:07:15 +0800

    deleted the new created table for user and just update the existing

--- app/Database/Migrations/2025-08-20-000323_CreateUsersTable.php
+++ app/Database/Migrations/2025-08-20-000323_CreateUsersTable.php
@@ -30,7 +30,7 @@ class CreateUsersTable extends Migration
             ],
             'role' => [
                 'type' => 'ENUM',
-                'constraint' => ['student', 'teacher', 'admin'],
+                'constraint' => ['admin', 'user'],
                 'default' => 'student',
             ],
             'created_at' => [
