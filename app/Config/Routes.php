13a210a69e5b913223dbda66f98f0b7343c7b9c8
jerica08 <jericamarquez88@gmail.com>
2025-09-01 15:11:22 +0800

    added configuation routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,16 +6,16 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-$routes->get('/register', 'Auth::register');
-$routes->post('/register', 'Auth::register');
-$routes->get('/login', 'Auth::login');
-$routes->post('/login', 'Auth::login');
-$routes->get('/logout', 'Auth::logout');
-$routes->get('/dashboard', 'Auth::dashboard');
 $routes->get('home', 'Home::index');
 $routes->get('/', 'Home::index');
 $routes->get('/about', 'Home::about');
 $routes->get('/contact', 'Home::contact');
 $routes->setAutoRoute(true);
 
-
+//Authentication Routes
+$routes->get('/auth/login', 'Auth::login');
+$routes->post('/auth/login', 'Auth::login');
+$routes->get('/auth/register', 'Auth::register');
+$routes->post('/auth/register', 'Auth::register');
+$routes->get('/auth/logout', 'Auth::logout');
+$routes->get('/dashboard', 'Auth::dashboard');
