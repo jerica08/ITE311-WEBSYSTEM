cf934cc3cb2fe1d1d3ec489aea666e4411065373
jerica08 <jericamarquez88@gmail.com>
2025-09-02 14:41:57 +0800

    delete to restart

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,11 +12,5 @@ $routes->get('/about', 'Home::about');
 $routes->get('/contact', 'Home::contact');
 $routes->setAutoRoute(true);
 
-// Authentication routes
-$routes->get('/register', 'Auth::register');
-$routes->post('/register', 'Auth::register');
-$routes->get('/login', 'Auth::login');
-$routes->post('/login', 'Auth::login');
-$routes->get('/logout', 'Auth::logout');
-$routes->get('/dashboard', 'Auth::dashboard');
+
 
