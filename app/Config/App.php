66c3bef66e65514318799884cccb6ede18ad6211
jerica08 <jericamarquez88@gmail.com>
2025-09-04 22:49:14 +0800

    modified

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,8 +16,10 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8000/';
- 
+    public string $baseURL = 'http://localhost/ITE311-MARQUEZ/public/';
+    // http://localhost/ITE311-MARQUEZ/public/
+    //http://localhost:8000/
+
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
      * If you want to accept multiple Hostnames, set this.
