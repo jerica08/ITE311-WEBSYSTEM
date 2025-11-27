1d39944eba88e546ae81af5ab195ef11553e3ca0
jerica08 <jericamarquez88@gmail.com>
2025-11-27 20:40:36 +0800

    modified user seeder

--- app/Database/Migrations/2025-08-20-000001_CreateUsersTable.php
+++ app/Database/Migrations/2025-08-20-000001_CreateUsersTable.php
@@ -43,7 +43,7 @@ class CreateUsersTable extends Migration
             ],
         ]);
         $this->forge->addPrimaryKey('id');
-        $this->forge->createTable('users');
+        $this->forge->createTable('users', true);
     }
 
     public function down()
